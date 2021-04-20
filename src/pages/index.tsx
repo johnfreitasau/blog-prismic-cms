34a7ac1f3892f23080fb1646fa283730d7c28127
John Freitas <john.freitasau@gmail.com>
2021-04-21 08:11:30 +1000

    Working on Prismic api

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,5 @@
+import Prismic from '@prismicio/client';
+import { RichText } from 'prismic-dom';
 import { GetStaticProps } from 'next';
 import Link from 'next/link';
 import { getPrismicClient } from '../services/prismic';
@@ -24,26 +26,64 @@ interface HomeProps {
   postsPagination: PostPagination;
 }
 
-export default function Home() {
+export default function Home({ posts }): JSX.Element {
   return (
     <>
       <div>
-        <Link href="">
+        {posts.map(post => (
           <a>
-            <strong>POST TITLE</strong>
-            <p>post subtitle</p>
-            <time>date and time</time>
-            <p>author</p>
+            <strong>{post.title}</strong>
+            <p>{post.subtitle}</p>
+            <time>{post.updatedDate}</time>
+            <p>{post.author}</p>
           </a>
-        </Link>
+        ))}
       </div>
     </>
   );
 }
 
-// export const getStaticProps = async () => {
-//   // const prismic = getPrismicClient();
-//   // const postsResponse = await prismic.query(TODO);
+export const getStaticProps = async () => {
+  const prismic = getPrismicClient();
+  const postsResponse = await prismic.query(
+    [Prismic.predicates.at('document.type', 'posts')],
+    {
+      fetch: ['post.title', 'post.content'],
+      pageSize: 100,
+    }
+  );
 
-//   // TODO
-// };
+  console.log('postsResponse:', postsResponse);
+  console.log('postsResponse:', JSON.stringify(postsResponse, null, 2));
+
+  const posts = postsResponse.results.map(post => {
+    return {
+      slug: post.uid,
+      // slug: 'slug',
+      // author: 'author',
+      author: post.data.author,
+      // author: RichText.asText(post.data.author),
+      title: RichText.asText(post.data.title),
+      // title: RichText.asText(post.data.title),
+      summary:
+        post.data?.content?.find(content => content.type === 'paragraph')
+          ?.text ?? '',
+      updatedDate: new Date(post.last_publication_date).toLocaleDateString(
+        'en-AU',
+        {
+          day: '2-digit',
+          month: 'long',
+          year: 'numeric',
+        }
+      ),
+    };
+  });
+
+  console.log('posts:', posts);
+
+  return {
+    props: {
+      posts,
+    },
+  };
+};
