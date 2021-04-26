317a80685b8884bf7549f9ff432b4a4386629f06
John Freitas <john.freitasau@gmail.com>
2021-04-26 20:38:10 +1000

    Working on Post page

--- src/pages/post/[slug].tsx
+++ src/pages/post/[slug].tsx
@@ -1,8 +1,9 @@
 import { GetStaticPaths, GetStaticProps } from 'next';
 
 import Prismic from '@prismicio/client';
+import { RichText } from 'prismic-dom';
+import { useRouter } from 'next/router';
 import { getPrismicClient } from '../../services/prismic';
-
 import commonStyles from '../../styles/common.module.scss';
 import Header from '../../components/Header';
 import styles from './post.module.scss';
@@ -28,15 +29,18 @@ interface PostProps {
   post: Post;
 }
 
-export default function Post({ post }: PostProps) {
-  // TODO
-  console.log('POST CLIENT:', post);
+export default function Post({ post }: PostProps): JSX.Element {
+  const router = useRouter();
+
+  if (router.isFallback) {
+    return <h2>Loading...</h2>;
+  }
 
   return (
     <>
       <Header />
       <div>
-        <img src={post.data.banner.url} alt="banner" />
+        <img src={post.data.banner.url} alt={post.data.title} />
       </div>
       <main>
         <article>
@@ -46,13 +50,19 @@ export default function Post({ post }: PostProps) {
             <span>John Freitas</span>
             <span>4 mins</span>
           </div>
-          <p>
-            <p>{post.data.content.heading}</p>
-            {/* <div
-              dangerouslySetInnerHTML={{ __html: post.data.content.heading }}
-              className={styles.postContent}
-            /> */}
-          </p>
+          <div>
+            {post.data.content.map(item => (
+              <div key={item.heading}>
+                {/* <h2>{item.heading}</h2> */}
+                <div
+                  dangerouslySetInnerHTML={{
+                    __html: RichText.asHtml(item.body),
+                  }}
+                  // className={styles.postContent}
+                />
+              </div>
+            ))}
+          </div>
         </article>
       </main>
     </>
@@ -63,24 +73,18 @@ export const getStaticPaths: GetStaticPaths = async () => {
   const prismic = getPrismicClient();
 
   const posts = await prismic.query(
-    [Prismic.Predicates.at('document.type', 'posts')],
-    {
-      fetch: ['post.uid'],
-      pageSize: 100,
-    }
+    [Prismic.Predicates.at('document.type', 'posts')]
+    // {
+    //   fetch: ['post.uid'],
+    //   pageSize: 100,
+    // }
   );
 
-  console.log('POSTS:', posts);
-
-  const paths = posts.results.map(post => {
-    return {
-      params: {
-        slug: post.uid,
-      },
-    };
-  });
-
-  console.log('PAThs:', paths);
+  const paths = posts.results.map(post => ({
+    params: {
+      slug: post.uid,
+    },
+  }));
 
   return {
     paths,
@@ -99,7 +103,23 @@ export const getStaticProps: GetStaticProps = async ({ params }) => {
 
   const { slug } = params;
 
-  const post = await prismic.getByUID('posts', String(slug), {});
+  const response = await prismic.getByUID('posts', String(slug), {});
+
+  const post = {
+    uid: response.uid,
+    first_publication_date: response.first_publication_date ?? null,
+    data: {
+      title: response.data.title,
+      banner: {
+        url: response.data.banner.url,
+      },
+      author: response.data.author,
+      content: response.data.content.map(item => ({
+        heading: item.heading,
+        body: item.body,
+      })),
+    },
+  };
 
   return {
     props: {
